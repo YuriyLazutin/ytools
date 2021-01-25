09903a606951b608802ada30e0137423e9153762
Yuriy Lazutin <yuriylazutin@gmail.com>
2021-01-25 21:05:00 +0700

    Added a message dialog declaration.

--- show-message/dialog.cpp
+++ show-message/dialog.cpp
@@ -1,8 +1,51 @@
 #include "dialog.h"
 
-Dialog::Dialog(QWidget *parent)
-    : QDialog(parent)
+Dialog::Dialog(QWidget *parent) : QDialog(parent)
 {
+
+    setObjectName(QString::fromUtf8("Dialog"));
+    resize(300, 120);
+    QSizePolicy szPolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    szPolicy.setHorizontalStretch(0);
+    szPolicy.setVerticalStretch(0);
+    szPolicy.setHeightForWidth(sizePolicy().hasHeightForWidth());
+    setSizePolicy(szPolicy);
+    setMinimumSize(QSize(300, 120));
+    setStyleSheet(QString::fromUtf8("background-color: qradialgradient(spread:pad, cx:0.5, cy:0.5, radius:1.5, fx:0.5, fy:0.5, stop:0 rgba(190, 200, 210, 255), stop:1 rgba(160, 200, 190, 223));"));
+
+    // Add vertical layout
+    verticalLayout = new QVBoxLayout(this);
+    verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+
+    // Add label
+    label = new QLabel(this);
+    label->setObjectName(QString::fromUtf8("label"));
+    label->setStyleSheet(QString::fromUtf8("background-color: rgba(0, 0, 0, 0);"));
+    label->setAlignment(Qt::AlignCenter);
+    verticalLayout->addWidget(label);
+
+    // Create horizontal layout
+    horizontalLayout = new QHBoxLayout();
+    horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+    horizontalSpacerLeft = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    horizontalLayout->addItem(horizontalSpacerLeft);
+    pushButton = new QPushButton(this);
+    pushButton->setObjectName(QString::fromUtf8("pushButton"));
+    pushButton->setMinimumSize(QSize(100, 30));
+    pushButton->setAutoFillBackground(false);
+    pushButton->setStyleSheet(QString::fromUtf8("background-color: rgb(148, 150, 148);"));
+    horizontalLayout->addWidget(pushButton);
+    horizontalSpacerRight = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    horizontalLayout->addItem(horizontalSpacerRight);
+
+    verticalLayout->addLayout(horizontalLayout);
+
+    setWindowTitle(QApplication::translate("Dialog", "Message", nullptr));
+    label->setText(QApplication::translate("Dialog", "Message Text", nullptr));
+    pushButton->setText(QApplication::translate("Dialog", "Ok", nullptr));
+
+    connect(pushButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+    //connect(pushButton, SIGNAL(released()), qApp, SLOT(quit()));
 }
 
 Dialog::~Dialog()
--- show-message/dialog.h
+++ show-message/dialog.h
@@ -2,12 +2,30 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QApplication>
+
+//#include <QtCore/QVariant>
+//
+//#include <QtWidgets/QDialog>
 
 class Dialog : public QDialog
 {
-    Q_OBJECT
+  Q_OBJECT
 
-public:
+  private:
+    QVBoxLayout *verticalLayout;
+    QLabel      *label;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacerLeft;
+    QPushButton *pushButton;
+    QSpacerItem *horizontalSpacerRight;
+
+  public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
 };
